c2a7bffadd42986bccc5baac511fbc6e494228ed
shmurthy024 <shreemurthy04@gmail.com>
2020-11-20 13:21:14 -0800

    UPDATED AFTER JENKINS JOB

--- numbers.sh
+++ numbers.sh
@@ -6,14 +6,14 @@
 echo "Please enter a positive integer: "
 read -r X
 N=1
-while [ $N -le $X  ]
+while [ $N -le "$X"  ]
 do
-	if [ $(($N%2))  -eq 0 ]
+	if [ $((N%2))  -eq 0 ]
 	then
 		echo "$N is even"
 	else
 		echo "$N is odd"
 	fi
-	N=$(($N+1))
+	N=$((N+1))
 	
 done
